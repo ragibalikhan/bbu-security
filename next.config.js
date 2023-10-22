ea30a255b7227e519510df6a3a31fbd42582d356
Ragib <ragibali84@gmail.com>
2023-10-22 18:58:41 +0530

    some-updates

--- next.config.js
+++ next.config.js
@@ -1,43 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export'
-    // async headers() {
-    //     return [
-    //       {
-    //         source: '/(.*)', // Apply these headers to all paths
-    //         headers: [
-    //             {
-    //                 key: 'Content-Security-Policy',
-    //                 value: " 'self'; 'self' 'unsafe-inline' style-src 'self' 'unsafe-inline';",
-    //               },
-    //         {
-    //             key: 'Referrer-Policy',
-    //             value: 'origin-when-cross-origin',
-    //           },
-    //           {
-    //             key: 'X-Content-Type-Options',
-    //             value: 'nosniff',
-    //           },
-    //           {
-    //             key: 'Permissions-Policy',
-    //             value: 'camera=(), microphone=(), geolocation=(), fullscreen=()',
-    //           },
-    //           {
-    //             key: 'X-Frame-Options',
-    //             value: 'DENY',
-    //           },
-    //           {
-    //             key: 'X-XSS-Protection',
-    //             value: '1; mode=block',
-    //           },
-    //           {
-    //             key: 'Strict-Transport-Security',
-    //             value: 'max-age=63072000; includeSubDomains; preload'
-    //           },
-    //         ],
-    //       },
-    //     ];
-    //   },
-    }
+    async headers() {
+        return [
+          {
+            source: '/(.*)', // Apply these headers to all paths
+            headers: [
+                {
+                    key: 'Content-Security-Policy',
+                    value: " 'self'; 'self' 'unsafe-inline' style-src 'self' 'unsafe-inline';",
+                  },
+            {
+                key: 'Referrer-Policy',
+                value: 'origin-when-cross-origin',
+              },
+              {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+              },
+              {
+                key: 'Permissions-Policy',
+                value: 'camera=(), microphone=(), geolocation=(), fullscreen=()',
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+              {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block',
+              },
+              {
+                key: 'Strict-Transport-Security',
+                value: 'max-age=63072000; includeSubDomains; preload'
+              },
+            ],
+          },
+        ];
+      },
+    };
 
 module.exports = nextConfig
