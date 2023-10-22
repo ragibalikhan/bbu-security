d54b9de17fba5c550c26359c3cdbc76a41e7d46e
Ragib <ragibali84@gmail.com>
2023-10-22 19:19:43 +0530

    security-update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,44 @@ const nextConfig = {
     // output: 'export'
     async headers() {
         return [
+            {
+                source: '/', // Apply these headers to all paths
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: " 'self'; 'self' 'unsafe-inline' style-src 'self' 'unsafe-inline';",
+                      },
+                {
+                    key: 'Referrer-Policy',
+                    value: 'origin-when-cross-origin',
+                  },
+                  {
+                    key: 'X-Content-Type-Options',
+                    value: 'nosniff',
+                  },
+                  {
+                    key: 'Permissions-Policy',
+                    value: 'camera=(), microphone=(), geolocation=(), fullscreen=()',
+                  },
+                  {
+                    key: 'X-Frame-Options',
+                    value: 'DENY',
+                  },
+                  {
+                    key: 'X-XSS-Protection',
+                    value: '1; mode=block',
+                  },
+                  {
+                    key: 'Strict-Transport-Security',
+                    value: 'max-age=63072000; includeSubDomains; preload'
+                  },
+                  {
+                    key: "Cache-Control",
+                    value: "public, max-age: 31536000, immutable",
+                },
+                ],
+                
+              },
           {
             source: '/(.*)', // Apply these headers to all paths
             headers: [
@@ -34,7 +72,12 @@ const nextConfig = {
                 key: 'Strict-Transport-Security',
                 value: 'max-age=63072000; includeSubDomains; preload'
               },
+              {
+                key: "Cache-Control",
+                value: "public, max-age: 31536000, immutable",
+            },
             ],
+            
           },
         ];
       },
